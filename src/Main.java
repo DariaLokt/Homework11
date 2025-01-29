85f375198e614fdc6ecb375726b83c5fb1f1aae2
DariaLokt <d.lokteeva@yandex.ru>
2025-01-29 14:09:35 +0500

    Done + extra

--- src/Main.java
+++ src/Main.java
@@ -61,7 +61,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Методы");
+        System.out.println("                                                      Методы");
         date();
         task(1);
         int year = 2000;
